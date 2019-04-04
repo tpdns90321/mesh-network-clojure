a27240749ad90d2a8bb71dfe3931f4f7b78f1dd0
tpdns90321 <tpdns9032100@gmail.com>
2019-04-04 10:46:46 +0900

    encoding/deserializer function test complete

--- test/mesh_network_clojure/encoding_test.clj
+++ test/mesh_network_clojure/encoding_test.clj
@@ -5,7 +5,9 @@
 
 (defn text-n-bytes [n]
   (let [n (- n 1)]
-    (apply (partial conj [(+ encoding/under-sized-text n)]) (repeat n 0))))
+    (if (encoding/big? n)
+      (apply (partial conj [encoding/over-sized-text (- n 1)]) (repeat (- n 1) 0))
+      (apply (partial conj [(+ encoding/under-sized-text n)]) (repeat n 0)))))
 
 (deftest deserializer
   "extract encoding element status"
@@ -23,4 +25,13 @@
   (testing "recursion list"
     (is (=
          (encoding/deserializer d/BYTEORDER (map #(+ encoding/under-sized-list %) [2 1 0]))
-         (list (list '(:List 1 3) (list '(:List 1 2) (list '(:List 1 1)))))))))
+         (list (list '(:List 1 3) (list '(:List 1 2) (list '(:List 1 1))))))))
+  (testing "long text"
+    (is (=
+         (encoding/deserializer d/BYTEORDER (text-n-bytes 100))
+         (list '(:Text 2 100)))))
+  (testing "long list"
+    (is (=
+         (encoding/deserializer d/BYTEORDER
+                                (concat (list encoding/over-sized-list 100) (text-n-bytes 100)))
+         (list '((:List 2 102) (:Text 2 100)))))))

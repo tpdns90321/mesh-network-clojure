16ff471537ac3e11cd9439eefe8aa5afddbaa414
tpdns90321 <tpdns9032100@gmail.com>
2019-04-16 13:15:03 +0900

    test for big-integer function

--- test/mesh_network_clojure/utils/bytes_test.clj
+++ test/mesh_network_clojure/utils/bytes_test.clj
@@ -11,3 +11,17 @@
     (is (nil? (b/bytes-to-int :big-endian :blabla))))
   (testing "not byte element"
     (is (nil? (b/bytes-to-int :big-endian (list :blabla 0 0 0))))))
+
+(deftest bytes-to-big-i128
+  (testing "check length limit"
+    (is
+      (nil? (b/bytes-to-i128
+              :big-endian (list 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10)))))
+  (testing "check range of unsigned bytes"
+    (is
+        (nil?
+          (b/bytes-to-i128 :big-endian (list 429 0 0)))))
+  (testing "not seq"
+    (is (nil? (b/bytes-to-i128 :big-endian :blabla))))
+  (testing "not byte element"
+    (is (nil? (b/bytes-to-i128 :big-endian (list :blabla 0 0 0))))))

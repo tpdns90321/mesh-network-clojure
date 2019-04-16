acc79b216b4d91f46daf4e597d546a5a94e848f5
tpdns90321 <tpdns9032100@gmail.com>
2019-04-16 09:44:44 +0900

    'bytes to' test

--- test/mesh_network_clojure/utils/bytes_test.clj
+++ test/mesh_network_clojure/utils/bytes_test.clj
@@ -6,4 +6,8 @@
   (testing "check length limit"
     (is (nil? (b/bytes-to-int :big-endian (list 1 0 0 0 0)))))
   (testing "check range of unsigned byte"
-    (is (nil? (b/bytes-to-int :big-endian (list 425 0 0 0))))))
+    (is (nil? (b/bytes-to-int :big-endian (list 425 0 0 0)))))
+  (testing "not seq"
+    (is (nil? (b/bytes-to-int :big-endian :blabla))))
+  (testing "not byte element"
+    (is (nil? (b/bytes-to-int :big-endian (list :blabla 0 0 0))))))
